f7aad56d51c09a20768f2e9e5751f67433373f33
Arda Soylu <ardasoylu39@hotmail.com>
2020-06-12 19:29:44 +0300

    hot fixes

--- koda/views.py
+++ koda/views.py
@@ -245,5 +245,7 @@ def onerilen_siteler(request):
     return render(request,'onerilen_siteler.html', {'context': context})
 
 def onerilen_siteler_detay(request, id):
-    context = get_object_or_404(SuggestedSites, site_type=id)
+    context = SuggestedSites.objects.all().filter(site_type=id)
+
+
     return render(request,'onerilen_siteler_detay.html', {'context': context})
